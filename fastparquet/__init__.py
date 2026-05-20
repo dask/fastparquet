42c9759dfe6e55e1857c750eaa12cb577f562453
Martin Durant <martin.durant@alumni.utoronto.ca>
2026-05-20 16:04:15 -0400

    fallback version

--- fastparquet/__init__.py
+++ fastparquet/__init__.py
@@ -1,8 +1,11 @@
 """parquet - read parquet files."""
 
-from fastparquet._version import __version__
 from fastparquet.writer import write, update_file_custom_metadata
 from fastparquet import core, schema, converted_types, api
 from fastparquet.api import ParquetFile
 from fastparquet.util import ParquetException
 
+try:
+    from fastparquet._version import __version__
+except ImportError:
+    __version__ = "unknown"
